ad054c2a0b0f621dcf0e98a1cc116c7c18ba62d4
Peter Weller <peter.weller@mailfence.com>
2022-03-31 03:49:40 +1100

    chore: display users list from api

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div className="app">🥔</div>;
-}
+import { FC } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { UsersList } from "./components/users-list";
+
+const queryClient = new QueryClient();
+
+const App: FC = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UsersList />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
