a50384e598a317561ecc5df7b44f0a7b790a0962
Peter Weller <contact@peterweller.com.au>
2022-03-31 03:08:57 +1100

    chore: add msw to example app

--- packages/example/src/main.tsx
+++ packages/example/src/main.tsx
@@ -2,10 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { setupWorker } from "msw";
+import { handlers } from "./mocks/handlers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const worker = setupWorker(...handlers);
+
+async function prepare() {
+  if (import.meta.env.DEV) {
+    await import("../mockServiceWorker.js?worker");
+
+    return worker.start({}).then(() => {
+      console.groupCollapsed("[MSW] Loaded with handlers 🎉");
+      worker.printHandlers();
+      console.groupEnd();
+      return null;
+    });
+  }
+}
+
+prepare().then(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
