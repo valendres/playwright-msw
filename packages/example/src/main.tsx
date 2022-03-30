b855462c0f96fd3c062b03dd46d7cb1a56260c5e
Peter Weller <peter.weller@mailfence.com>
2022-03-31 03:17:13 +1100

    chore: prune example app

--- packages/example/src/main.tsx
+++ packages/example/src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { setupWorker } from "msw";
 import { handlers } from "./mocks/handlers";
