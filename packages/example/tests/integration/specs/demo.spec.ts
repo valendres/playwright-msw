59b6404229922c934d18a0ec5909ee06dc7e9540
Peter Weller <contact@peterweller.com.au>
2022-03-31 14:45:45 +1100

    chore: get example tests to work

---
+++ packages/example/tests/integration/specs/demo.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from "../test";
+
+test.describe.parallel("demo", () => {
+  test("should use the default handlers without requiring handlers to be specified on a per-test basis", async ({
+    page,
+  }) => {
+    await page.goto("/");
+    await expect(page.locator('text="Alessandro Metcalfe"')).toBeVisible();
+  });
+
+  test.only("should allow mocks to be overridden on a per test basis", async ({
+    page,
+    msw,
+    rest,
+  }) => {
+    await msw.use(
+      rest.get("/api/users", (_, response, context) =>
+        response(context.delay(250), context.status(403))
+      )
+    );
+    await page.goto("/");
+    await expect(page.locator('text="Alessandro Metcalfe"')).toBeHidden();
+    await expect(page.locator('text="Failed to load users"')).toBeVisible();
+  });
+});
--- packages/example/tests/integration/specs/demo.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "../test";
-
-test.describe.parallel("demo", () => {
-  test("should load a page", async ({ page }) => {
-    await page.goto("/");
-  });
-});
