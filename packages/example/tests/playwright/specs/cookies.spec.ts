5e1a30a349e7ef238c828a8f67044b91ac9a5d71
Peter Weller <peter.weller@mailfence.com>
2022-11-06 06:05:22 +1100

    chore: improve test performance

--- packages/example/tests/playwright/specs/cookies.spec.ts
+++ packages/example/tests/playwright/specs/cookies.spec.ts
@@ -1,8 +1,7 @@
 import { LoginForm } from "../models/login-form";
 import { test } from "../test";
 
-// NOTE: tests are intentionally sequential, do not use `test.describe.parallel` here
-test.describe.serial("cookies", () => {
+test.describe.parallel("cookies", () => {
   test("should not have a session cookie if the user has not logged in yet", async ({
     page,
   }) => {
@@ -10,16 +9,6 @@ test.describe.serial("cookies", () => {
 
     const loginForm = new LoginForm(page);
     await loginForm.assertSessionStatus(401);
-  });
-
-  test("should have a valid session once the user logs in", async ({
-    page,
-  }) => {
-    await page.goto("/login");
-
-    const loginForm = new LoginForm(page);
-    await loginForm.loginWithValidCredentials();
-    await loginForm.assertSessionStatus(200);
   });
 
   test("should still have a valid session if the user refreshes the page after logging in", async ({
@@ -36,15 +25,6 @@ test.describe.serial("cookies", () => {
     await loginForm.assertSessionStatus(200);
   });
 
-  test("should not have a session from the previous test run", async ({
-    page,
-  }) => {
-    await page.goto("/login");
-
-    const loginForm = new LoginForm(page);
-    await loginForm.assertSessionStatus(401);
-  });
-
   test("should allow the user to clear their session by logging out", async ({
     page,
   }) => {
@@ -55,5 +35,26 @@ test.describe.serial("cookies", () => {
     await loginForm.assertSessionStatus(200);
     await loginForm.logout();
     await loginForm.assertSessionStatus(401);
+  });
+
+  test.describe.serial("when running sequentially", () => {
+    test("should have a valid session once the user logs in", async ({
+      page,
+    }) => {
+      await page.goto("/login");
+
+      const loginForm = new LoginForm(page);
+      await loginForm.loginWithValidCredentials();
+      await loginForm.assertSessionStatus(200);
+    });
+
+    test("should not have a session from the previous test run", async ({
+      page,
+    }) => {
+      await page.goto("/login");
+
+      const loginForm = new LoginForm(page);
+      await loginForm.assertSessionStatus(401);
+    });
   });
 });
